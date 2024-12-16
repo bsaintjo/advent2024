6edfe3d32adf965986eb9a0574c4880c4203b747
Brandon Saint-John <bsaintjo@ucsc.edu>
2024-12-16 08:09:47 -0800

    Finish day 4

---
+++ days/day-04/src/bin/part02.rs
@@ -0,0 +1,11 @@
+use std::io::Read;
+
+use advent2024::open_file_cmd_line;
+
+fn main() {
+    let mut file = open_file_cmd_line();
+    let mut buf = String::new();
+    file.read_to_string(&mut buf).unwrap();
+    let puzzle = day_04::Puzzle::from_iter(buf.split_whitespace());
+    println!("{}", puzzle.count_x_mas());
+}
--- days/day-04/src/lib.rs
+++ days/day-04/src/lib.rs
@@ -8,13 +8,31 @@ struct WordSearch {
     col: usize,
 }
 
+type CrossPosition = (Vec<(usize, usize)>, Vec<(usize, usize)>);
+
 impl WordSearch {
-    fn calc_vectors(&self, puzzle: &Puzzle) -> Vec<Vec<(usize, usize)>> {
+    fn calc_vectors_cross(&self, puzzle: &Puzzle) -> Option<CrossPosition> {
+        let left_ok = self.col > 0;
+        let up_ok = self.row > 0;
+        let right_ok = (self.col + 1) < puzzle.n_cols();
+        let down_ok = (self.row + 1) < puzzle.n_rows();
+
+        if left_ok && up_ok && right_ok && down_ok {
+            let fst = vec![(self.row - 1, self.col - 1), (self.row, self.col), (self.row + 1, self.col + 1)];
+            let snd = vec![(self.row - 1, self.col + 1), (self.row, self.col), (self.row + 1, self.col - 1)];
+            Some((fst, snd))
+        } else {
+            None
+        }
+
+    }
+
+    fn calc_vectors_eight_way(&self, puzzle: &Puzzle) -> Vec<Vec<(usize, usize)>> {
         let mut acc = Vec::new();
         let left_ok = self.col.checked_sub(3).is_some();
         let up_ok = self.row.checked_sub(3).is_some();
-        let right_ok = (self.col + 4) < puzzle.n_cols();
-        let down_ok = (self.row + 4) < puzzle.n_rows();
+        let right_ok = (self.col + 3) < puzzle.n_cols();
+        let down_ok = (self.row + 3) < puzzle.n_rows();
 
         if left_ok {
             acc.push(repeat(self.row).zip(self.col - 3..=self.col).collect_vec());
@@ -95,11 +113,11 @@ impl Puzzle {
         self.letters.len()
     }
 
-    fn get_starts(&self) -> Vec<WordSearch> {
+    fn get_starts(&self, start: u8) -> Vec<WordSearch> {
         let mut acc = Vec::new();
         for (row_idx, row) in self.letters.iter().enumerate() {
             for (col_idx, &col) in row.iter().enumerate() {
-                if col == b'X' {
+                if col == start {
                     acc.push(WordSearch {
                         row: row_idx,
                         col: col_idx,
@@ -115,11 +133,29 @@ impl Puzzle {
             .collect()
     }
 
+    pub fn count_x_mas(&self) -> usize {
+        let mut count = 0;
+        let wss = self.get_starts(b'A');
+        for ws in wss {
+            if let Some((fst, snd)) = ws.calc_vectors_cross(self) {
+                let fst = self.spell(&fst);
+                let snd = self.spell(&snd);
+
+                let fst_valid = (fst == b"MAS") || (fst == b"SAM");
+                let snd_valid = (snd == b"MAS") || (snd == b"SAM");
+                if fst_valid && snd_valid {
+                    count += 1;
+                }
+            }
+        }
+        count
+    }
+
     pub fn count_xmas(&self) -> usize {
         let mut count = 0;
-        let wss = self.get_starts();
+        let wss = self.get_starts(b'X');
         for ws in wss {
-            let valid_idxs = ws.calc_vectors(self);
+            let valid_idxs = ws.calc_vectors_eight_way(self);
             let mut local_count = 0;
             for valid_idx in valid_idxs {
                 let res = String::from_utf8(self.spell(&valid_idx)).unwrap();
@@ -127,12 +163,32 @@ impl Puzzle {
                     local_count += 1;
                 }
             }
-            // if local_count > 0 {
-            //     println!("{ws:?}, {local_count}");
-            // }
             count += local_count;
         }
         count
+    }
+}
+
+struct Windows<'a> {
+    row: usize,
+    col: usize,
+    length: usize,
+    width: usize,
+    puzzle: &'a Puzzle
+}
+
+impl<'a> Iterator for Windows<'a> {
+    type Item = &'a [&'a [u8]];
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if (self.col + self.width) >= self.puzzle.n_cols() && ((self.row + self.length) >= self.puzzle.n_rows()){
+            return None;
+        }
+        if (self.col + self.width) >=self.puzzle.n_cols() {
+            self.col = 0;
+            self.row += 1;
+        }
+        todo!()
     }
 }
 
@@ -141,7 +197,57 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_part01() {
+    fn test_x_mas() {
+        let letters = "M.M
+.A.
+S.S";
+        let puzzle = Puzzle::from_iter(letters.split_whitespace());
+        assert_eq!(puzzle.count_x_mas(), 1);
+    }
+
+    #[test]
+    fn test_top_left_corner() {
+        let letters = "XMAS
+MM..
+A.A.
+S..S";
+        let puzzle = Puzzle::from_iter(letters.split_whitespace());
+        assert_eq!(puzzle.count_xmas(), 3);
+    }
+
+    #[test]
+    fn test_bottom_left_corner() {
+        let letters = "S..S
+A.A.
+MM..
+XMAS";
+        let puzzle = Puzzle::from_iter(letters.split_whitespace());
+        assert_eq!(puzzle.count_xmas(), 3);
+    }
+
+    #[test]
+    fn test_top_right_corner() {
+        let letters = "SAMX
+..MM
+.A.A
+S..S";
+        let puzzle = Puzzle::from_iter(letters.split_whitespace());
+        assert_eq!(puzzle.count_xmas(), 3);
+    }
+
+    #[test]
+    fn test_bottom_right_corner() {
+        let letters = "S..S
+.A.A
+..MM
+SAMX";
+        let puzzle = Puzzle::from_iter(letters.split_whitespace());
+        assert_eq!(puzzle.count_xmas(), 3);
+    }
+
+
+    #[test]
+    fn test_part0x() {
         let letters = "MMMSXXMASM
 MSAMXMSMSA
 AMXSXMAAMM
@@ -153,28 +259,7 @@ SAXAMASAAA
 MAMMMXMMMM
 MXMXAXMASX";
         let puzzle = Puzzle::from_iter(letters.split_whitespace());
-        let ws = WordSearch { row: 5, col: 9 };
-
-        assert_eq!(
-            String::from_utf8(puzzle.spell(&[(1, 4), (1, 3), (1, 2), (1, 1)])).unwrap(),
-            "XMAS"
-        );
-        let mut count = 0;
-        let wss = puzzle.get_starts();
-        for ws in wss {
-            let valid_idxs = ws.calc_vectors(&puzzle);
-            let mut local_count = 0;
-            for valid_idx in valid_idxs {
-                let res = String::from_utf8(puzzle.spell(&valid_idx)).unwrap();
-                if (res == "XMAS") || (res == "SAMX") {
-                    local_count += 1;
-                }
-            }
-            if local_count > 0 {
-                println!("{ws:?}, {local_count}");
-            }
-            count += local_count;
-        }
-        println!("{count}");
+        assert_eq!(puzzle.count_xmas(), 18);
+        assert_eq!(puzzle.count_x_mas(), 9);
     }
 }
